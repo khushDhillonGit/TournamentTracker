b804b80e9e82f3668db23f986d73b6614049c401
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-05-24 21:17:38 -0400

    added Model classes for building the application

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Represents One Team in the Matchup
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Represents score for this team
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Represents the mathcup from where this team came as winner
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+
+    }
+}
