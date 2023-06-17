122fc7c59dc12f4a1f84f71c27752e95fb991b8b
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-06-17 19:11:19 -0400

    created data access class and models

---
+++ TrackerLibrary/DataAccess/SqlConnecter.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+using System.Data.SqlClient;
+using Dapper;
+namespace TrackerLibrary.DataAccess
+{
+    public class SqlConnecter : IDataConection
+    {
+        //TODO - make create prize method actually save
+        /// <summary>
+        ///  Saves a new Prize
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>The Prize information</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+
+            using (IDbConnection connection = new SqlConnection(GlobalConfig.CnnString("Tournaments"))) 
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber",model.PlaceNumber);
+                p.Add("@PlaceName",model.PlaceName);
+                p.Add("@PrizePercentage",model.PrizePercentage);
+                p.Add("@PrizeAmount",model.PrizeAmount);
+                p.Add("@id", 0, dbType: DbType.Int32,direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@id");
+
+                return model;
+            }
+                
+        }
+    }
+}
---
+++ TrackerLibrary/DataAccess/TextConnecter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Models;
+
+namespace TrackerLibrary.DataAccess
+{
+    public class TextConnecter : IDataConection
+    {
+        //TODO - wire up createPrize text files
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            return new PrizeModel();
+        }
+    }
+}
