a0334f16f093d2a6db6e2713f3d0152e67b6d969
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-06-17 19:10:10 -0400

    models in place and properlt set up

---
+++ TrackerLibrary/Models/PrizeModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary.Models
+{
+    public class PrizeModel
+    {
+        public int Id { get; set; }
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PrizeAmount { get; set; }
+        public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        {
+
+        }
+
+        public PrizeModel(string placeName, string placeNumber, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = placeName;
+
+            _ = int.TryParse(placeNumber, out int placeNumberValue);
+            PlaceNumber = placeNumberValue;
+
+            _ = decimal.TryParse(prizeAmount, out decimal prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            _ = double.TryParse(prizePercentage, out double prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+        }
+    }
+}
