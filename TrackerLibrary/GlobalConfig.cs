dfb834742e606ead79eb967362724046ea4fcf2d
Khushwinder Singh <khushwindersinghdhillon79@gmail.com>
2023-06-17 19:11:37 -0400

    added global config method to connect to databse

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.DataAccess;
+using System.Configuration;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConection> Connections { get; private set; } = new List<IDataConection>();
+
+        public static void InitializeConnections(bool database, bool textFiles) 
+        {
+            if (database) 
+            { 
+                // TODO SetUpSQl Connecter properly
+                SqlConnecter sql = new SqlConnecter();
+                Connections.Add(sql);
+            }
+
+            if (textFiles) 
+            { 
+                //TODO  setup text file properly
+                TextConnecter textConnecter = new TextConnecter();
+                Connections.Add(textConnecter);
+            }
+        }
+        public static string CnnString(string name) { 
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+
+    
+
+
+}
